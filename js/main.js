cf1355b38152ec3ad888a8f0890624a3bc94abee
Jacob John <jacobjohn90@gmail.com>
2018-05-06 01:29:59 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+	];
+
 var cardsInPlay = [];
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -9,8 +31,10 @@ console.log("Sorry, try again.");
 }
 }
 var flipCard = function (cardId) {
-	console.log ("User flipped " + cards[cardId]);
-	cardsInPlay.push(cardId)
+	console.log ("User flipped " + cards[cardId].rank);
+	console.log (cards[cardId].cardImage);
+	console.log (cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank)
  if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
  	alert("You found a match!");	
  } else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
@@ -18,6 +42,6 @@ var flipCard = function (cardId) {
 }
 checkForMatch();
  }
- 
+
  flipCard(0);
  flipCard(2);
